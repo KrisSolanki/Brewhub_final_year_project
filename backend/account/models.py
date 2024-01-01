f4b5b3b413b21744fa26ca168a655ab8709d7076
KrisSolanki <krissolanki28@gmail.com>
2024-01-02 01:27:25 +0530

    Commit_2_migration

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = None
+    mobile_no = models.CharField(max_length=10,unique=True)
+    email = models.CharField(max_length=50) 
+
+    USERNAME_FIELD = 'mobile_no'
+    REQUIRED_FIELDS = []
