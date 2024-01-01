e23599852a7186f478371bdfaa979afa46ace707
KrisSolanki <krissolanki28@gmail.com>
2024-01-02 03:54:24 +0530

    commit_3

--- backend/account/models.py
+++ backend/account/models.py
@@ -8,7 +8,8 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=30)
     username = None
     mobile_no = models.CharField(max_length=10,unique=True)
-    email = models.CharField(max_length=50) 
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=50,blank=True,null=True) 
 
     USERNAME_FIELD = 'mobile_no'
     REQUIRED_FIELDS = []
