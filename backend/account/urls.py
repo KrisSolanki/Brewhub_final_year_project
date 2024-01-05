30725dcfbfc8b103f6440f99d57824d3a37164bd
KrisSolanki <krissolanki28@gmail.com>
2024-01-05 21:39:18 +0530

    Password_reset DONE

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path,include
 from .views import RegisterView,LoginView
 from account.views import * #----------- date : 3/01/2024------------
+from django.contrib.auth import views as auth_views #----------------------date : 5/01/2024 for reset_password , * related to password
+from django.core.mail.backends.smtp import EmailBackend #------------date : 5/01/2024 for reset_password
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -17,6 +19,14 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('send_otp/',send_otp),
     path('verify_otp/',verify_otp),
-    path('resend_otp/',resend_otp)
+    path('resend_otp/',resend_otp),
+
+    
+    #-------------- date : 5/01/2024 ---- reset password -----
+    path('reset_password/',auth_views.PasswordResetView.as_view(),name='reset_password'), #Submit email form  //PasswordResetView.as_view()
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'), #Emal sent success message //PasswordResetDoneView.as_view()
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'), #link to password reset form in email //PasswordResetConfirmView.as_view()
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete') #password successfully changed message //PasswordResetCompleteView.as_view()
+
 
 ]
