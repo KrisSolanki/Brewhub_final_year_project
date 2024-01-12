b3df8501defba6fb4bc17a561059857840d356ac
KrisSolanki <krissolanki28@gmail.com>
2024-01-12 10:48:48 +0530

    simple commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -198,8 +198,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ktheking280@gmail.com'
-EMAIL_HOST_PASSWORD = 'tceg lvca spgl bnsr'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 PROFILE_MEDIA_URL = '/media/profile'
 PROFILE_MEDIA_ROOT = BASE_DIR / "media/profile"
