8dfeba2caabad33c3a049d6d71f29f64b9ebef45
KrisSolanki <krissolanki28@gmail.com>
2024-02-29 02:43:06 +0530

    some changes added

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'; 
+// import React, { useState, useEffect } from 'react'; 
+import React from 'react'; 
 import './Navbar.css' ;
 import Logo from '../../Assets/NavbarImgs/test1.png' ;
 import Search from '../../Assets/NavbarImgs/search.png';
@@ -13,48 +14,53 @@ import { FaShoppingCart } from "react-icons/fa";
 
 import { FaCircleUser } from "react-icons/fa6";
 
+// import { FaBars } from "react-icons/fa6";
+
+
 
 
 const Navbar = () => {
     
-    const [isScrolled, setIsScrolled] = useState(false);
+  //   const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY > 0) {
+  //       setIsScrolled(true);
+  //     } else {
+  //       setIsScrolled(false);
+  //     }
+  //   };
 
-    document.addEventListener('scroll', handleScroll);
+  //   document.addEventListener('scroll', handleScroll);
 
-    // Cleanup the event listener on component unmount
-    return () => {
-      document.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   // Cleanup the event listener on component unmount
+  //   return () => {
+  //     document.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
   
     //   document.addEventListener('scroll', handleScroll);
   return (
     <>
-    {/* <div className='container'> */}
-    <div className={`container ${isScrolled ? 'scrolled' : ''}`}>
+    <div className='container'>
+    {/* <div className={`container ${isScrolled ? 'scrolled' : ''}`}> */}
         <div className="logo">
             <div className="logostl">
             <img src={Logo} alt="Logoimg" className="logo-img" />
             </div>
             <p>BREWHUB</p>
         </div>
-        <div className="lists">
-            <ul>
-                <li>
+        <div className="lst">
+            {/* <ul className='srch'> */}
+                {/* <li> */}
                     <div className="searchbox">
                     <input type="text" placeholder="Search" className='searchtxt'/>
                     <button className="btn1" id="btn1" type="submit"><img src={Search} alt="search img" className='search-img'/></button>
-        </div>
-                </li>
+                    </div>
+                {/* </li> */}
+                {/* </ul> */}
+                <ul className='tgl'>
                 <li><a className='active' href='https://www.google.com/'><span><IoIosHome /></span>Home</a></li>
                 <li><a className='' href='www.youtube.com'><span><MdGroups /></span>About us</a></li>
                 <li><a className='' href='www.yahoo,com'><span><RiLoginBoxFill /></span>Sign in</a></li>
@@ -62,6 +68,8 @@ const Navbar = () => {
                 <li><a className='' href='www.instagram.com'><span><FaCircleUser /></span>Profile</a></li>
             </ul>
         </div>
+                {/* <div className="toggleBtn"><i><FaBars /></i></div> */}
+                
         
       
     </div>
